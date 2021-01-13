d260af2990d07b536ff5a8365daf0fda6cfc8a74
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-01-13 23:37:08 +0100

    Add CodingJS-Solutions project
    Created new Project CodingJS-Solutions to post solutions to the-winter.github.io problems.
    Added index.html and its basic structure.
    Added empty index.js
    Created package.json
    Added first 5 solutions to problems.

---
+++ CodingJS-Solutions/solutions/diff21.js
@@ -0,0 +1,15 @@
+/*****************************
+// Warmup-1 -- diff21
+// Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+// Examples
+
+// diff21(19) → 2
+// diff21(10) → 11
+// diff21(21) → 0
+*****************************/
+
+function diff21(n){
+  let double_over21 = (n > 21) + 1
+  return Math.abs(n - 21) * double_over21
+}
