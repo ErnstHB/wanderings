db4dc01ffa776bc23f5dd678b4caa4fe5c88fb63
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-01-14 21:24:11 +0100

    new file:   solutions/makes10.js 	new file:   solutions/missingChar.js 	new file:   solutions/nearHundred.js 	new file:   solutions/notString.js 	new file:   solutions/posNeg.js

---
+++ CodingJS-Solutions/solutions/nearHundred.js
@@ -0,0 +1,16 @@
+/*****************************
+Warmup-1 -- nearHundred
+Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+Examples
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false
+*****************************/
+
+function nearHundred(n, a){
+  let nearOneHundred = Math.abs(n - 100)
+  let nearTwoHundred = Math.abs(n - 200)
+  return(nearOneHundred <= 10 || nearTwoHundred <= 10)
+}
