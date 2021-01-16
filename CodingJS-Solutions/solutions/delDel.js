add4b0490e946c732ccbfc4d5abdd6169d51d030
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-01-16 20:54:29 +0100

    3 more codingJs solutions

---
+++ CodingJS-Solutions/solutions/delDel.js
@@ -0,0 +1,19 @@
+/*****************************
+Warmup-1 -- delDel
+Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+
+Examples
+
+delDel('adelbc') → abc
+delDel('adelHello') → aHello
+delDel('abcdel') → abcdel
+*****************************/
+
+function delDel(str){
+  let noDelStr
+  if (str.substr(1,3) == "del")
+    noDelStr = (str.substr(0,1) + str.substr(4,str.length-3))
+  else
+    noDelStr = str
+  return noDelStr
+}
---
+++ CodingJS-Solutions/solutions/mixStart.js
@@ -0,0 +1,14 @@
+/*****************************
+Warmup-1 -- mixStart
+Return true if the given string begins with 'mix', except the 'm' can be anything, so 'pix', '9ix' .. all count.
+
+Examples
+
+mixStart('mix snacks') → true
+mixStart('pix snacks') → true
+mixStart('piz snacks') → false
+*****************************/
+
+function mixStart(str){
+  return(str.substr(1,2) == "ix")
+}
