1175a86200f866a7a6f9c7412c599ff3f72dda9b
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-02-05 20:27:51 +0100

    Strings 2 started

---
+++ CodingJS-Solutions/solutions/04.String-2/catDog.js
@@ -0,0 +1,22 @@
+/*****************************
+String-2 -- catDog
+Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+Examples
+
+catDog('catdog') → true
+catDog('catcat') → false
+catDog('1cat1cadodog') → true
+*****************************/
+
+function catDog(str){
+  let cats = countWord('cat', str);
+  let dogs = countWord('dog', str);
+  return cats == dogs;
+}
+
+function countWord(word, str, start = 0, count = 0){
+  var pos = str.substr(start).search(word);
+  return (count == str.length || pos == -1) ?
+         count : countWord(word, str, start+pos+word.length, count+1);
+}
---
+++ CodingJS-Solutions/solutions/04.String-2/countHi.js
@@ -0,0 +1,16 @@
+/*****************************
+String-2 -- countHi
+Return the number of times that the string "hi" appears anywhere in the given string.
+
+Examples
+
+countHi('abc hi ho') → 1
+countHi('ABChi hi') → 2
+countHi('hihi') → 2
+*****************************/
+
+function countHi(str, index = 0, count = 0){
+  var pos = str.substr(index).search('hi');
+  return (count == str.length || pos == -1) ?
+         count : countHi(str, index+pos+2, count+1);
+}
