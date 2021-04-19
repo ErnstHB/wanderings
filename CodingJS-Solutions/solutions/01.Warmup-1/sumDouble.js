e7f016b22953d26d773c856b44c86c963efd894d
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-04-19 16:04:50 +0200

    Update sumDouble.js

--- CodingJS-Solutions/solutions/01.Warmup-1/sumDouble.js
+++ CodingJS-Solutions/solutions/01.Warmup-1/sumDouble.js
@@ -1,16 +1,16 @@
 /*****************************
-// Warmup-1 -- sumDouble
-// Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+Warmup-1 -- sumDouble
+Given two int values, return their sum. Unless the two values are the same, 
+then return double their sum.
 
-// Examples
+Examples
 
-// sumDouble(1, 2) → 3
-// sumDouble(3, 2) → 5
-// sumDouble(2, 2) → 8
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
 *****************************/
 
 function sumDouble(a, b){
   let same = (a == b) + 1;
   return ((a + b) * same);
-  return Math.max
 }
